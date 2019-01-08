ba376ac537338164121dbb376627e39f0cef7324
larda3@rsd <larda3@rsd>
2019-01-08 14:52:07 +0000

    modified ListCollector.py for the new interface
    Author: mr

--- ListCollector.py
+++ ListCollector.py
@@ -19,7 +19,7 @@ print(camp_list)
 
 
 for cname in camp_list:
-    larda=pyLARDA.LARDA(cname, build_lists=True)
+    larda=pyLARDA.LARDA().connect(cname, build_lists=True)
     
 #larda=pyLARDA.LARDA('COLRAWI')
 #larda=pyLARDA.LARDA('LACROS_at_Leipzig', build_lists=True)
