99a57d9bc72a4195a34404e72759745b1704c909
willi <willischimmel@gmx.de>
2019-01-09 20:32:35 +0100

    added scatter plot to Transformation.py

---
+++ examples/scatter_Z_mdv.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import sys
+
+# just needed to find pyLARDA from this location
+sys.path.append('../')
+sys.path.append('.')
+
+import matplotlib
+
+matplotlib.use('Agg')
+
+import pyLARDA
+import datetime
+
+# Load LARDA
+larda = pyLARDA.LARDA().connect('lacros_dacapo')
+c_info = [larda.camp.LOCATION, larda.camp.VALID_DATES]
+
+print(larda.days_with_data())
+
+# begin_dt=datetime.datetime(2018,12,6,0,1)
+begin_dt = datetime.datetime(2018, 12, 6, 1, 40, 0)
+end_dt   = datetime.datetime(2018, 12, 6, 4, 0,  0)
+
+plot_range = [300, 10000]
+case_prefix = 'plots/scatter_case_studies/comparison_cloud_'
+
+# load the reflectivity data
+MIRA_Zg = larda.read("MIRA", "Zg", [begin_dt, end_dt], [0, 'max'])
+MIRA_Zg['var_lims'] = [-60, 20]
+
+LIMRAD94_Z = larda.read("LIMRAD94", "Ze", [begin_dt, end_dt], [0, 'max'])
+LIMRAD94_Z['var_lims'] = [-60, 20]
+
+LIMRAD94_Z_interp = pyLARDA.Transformations.interpolate2d(LIMRAD94_Z, new_time=MIRA_Zg['ts'], new_range=MIRA_Zg['rg'])
+
+fig, ax = pyLARDA.Transformations.scatter(MIRA_Zg, LIMRAD94_Z_interp, var_lim=[-75, 20],
+                                          custom_offset_lines=5.0, z_converter='lin2z')
+
+fig.savefig(case_prefix + 'scatter_mira_limrad.png', dpi=250)
--- pyLARDA/Transformations.py
+++ pyLARDA/Transformations.py
@@ -2,15 +2,20 @@
 
 
 import datetime
-#import itertools
+# import itertools
 import copy
 
 import numpy as np
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import scipy.interpolate
 
+# scientific python imports
+import scipy.interpolate
+from scipy import stats
+
+import pyLARDA
 import pyLARDA.VIS_Colormaps as VIS_Colormaps
 import pyLARDA.helpers as h
 
@@ -33,12 +38,12 @@ def join(datadict1, datadict2):
     assert datadict1['type'] == datadict2['type']
     new_data['type'] = datadict1['type']
     container_type = datadict1['type']
-    
+
     if container_type == "timeheight":
         print(datadict1['ts'].shape, datadict1['rg'].shape, datadict1['var'].shape)
         print(datadict2['ts'].shape, datadict2['rg'].shape, datadict2['var'].shape)
     thisjoint = datadict1['ts'].shape[0]
-    new_data["joints"] = datadict1.get('joints',[]) + [thisjoint] + datadict2.get('joints',[])
+    new_data["joints"] = datadict1.get('joints', []) + [thisjoint] + datadict2.get('joints', [])
     print("joints", new_data['joints'])
     new_data['filename'] = h.flatten([datadict1['filename']] + [datadict2['filename']])
     print(new_data['filename'])
@@ -61,7 +66,6 @@ def join(datadict1, datadict2):
     new_data['name'] = datadict1['name']
     print(new_data['type'])
     print(new_data['paraminfo'])
-
 
     if container_type == "timeheight" \
             or container_type == "timeheightspec":
@@ -93,13 +97,13 @@ def interpolate2d(data, mask_thres=0.1, **kwargs):
 
     kx, ky = 1, 1
     interp_var = scipy.interpolate.RectBivariateSpline(
-        data['ts'], data['rg'], var, 
+        data['ts'], data['rg'], var,
         kx=kx, ky=ky)
     interp_mask = scipy.interpolate.RectBivariateSpline(
         data['ts'], data['rg'], data['mask'].astype(np.float),
         kx=kx, ky=ky)
-    print(data['mask'].astype(np.float)[:,10])
-    print(data['mask'][:,10])
+    print(data['mask'].astype(np.float)[:, 10])
+    print(data['mask'][:, 10])
 
     new_time = data['ts'] if not 'new_time' in kwargs else kwargs['new_time']
     new_range = data['rg'] if not 'new_range' in kwargs else kwargs['new_range']
@@ -133,12 +137,12 @@ def combine(func, datalist, keys_to_update, **kwargs):
         datalist: list of data containers
         keys_to_update: dictionary of keys to update
     """
-    
+
     if len(datalist) > 1:
-        assert np.all(datalist[0]['rg'] == datalist[1]['rg']) 
+        assert np.all(datalist[0]['rg'] == datalist[1]['rg'])
         assert np.all(datalist[0]['ts'] == datalist[1]['ts'])
 
-    #use the first dict as the base
+    # use the first dict as the base
     new_data = {**datalist[0]}
     new_data.update(keys_to_update)
 
@@ -147,7 +151,7 @@ def combine(func, datalist, keys_to_update, **kwargs):
         'filename': [e['filename'] for e in datalist],
         'paraminfo': [e['paraminfo'] for e in datalist],
     }
-    
+
     return new_data
 
 
@@ -170,64 +174,64 @@ def plottimeseries(data, **kwargs):
     dt_list = [datetime.datetime.utcfromtimestamp(time) for time in time_list]
     # this is the last valid index
     var = var.filled(-999)
-    jumps = np.where(np.diff(time_list)>60)[0]
+    jumps = np.where(np.diff(time_list) > 60)[0]
     for ind in jumps[::-1].tolist():
-        print("jump at ", ind, dt_list[ind-1:ind+2])
+        print("jump at ", ind, dt_list[ind - 1:ind + 2])
         # and modify the dt_list
-        dt_list.insert(ind+1, dt_list[ind]+datetime.timedelta(seconds=5))
+        dt_list.insert(ind + 1, dt_list[ind] + datetime.timedelta(seconds=5))
         # add the fill array
-        var = np.insert(var, ind+1, -999, axis=0)
-    
-    var = np.ma.masked_equal(var, -999) 
+        var = np.insert(var, ind + 1, -999, axis=0)
+
+    var = np.ma.masked_equal(var, -999)
 
     fig, ax = plt.subplots(1, figsize=(10, 5.7))
     vmin, vmax = data['var_lims']
-    print("varlims", vmin,vmax)
-    if 'z_converter' in kwargs: 
+    print("varlims", vmin, vmax)
+    if 'z_converter' in kwargs:
         if kwargs['z_converter'] == 'log':
-            #plotkwargs['norm'] = matplotlib.colors.LogNorm(vmin=vmin, vmax=vmax)
+            # plotkwargs['norm'] = matplotlib.colors.LogNorm(vmin=vmin, vmax=vmax)
             ax.set_yscale('log')
         else:
             var = h.get_converter_array(kwargs['z_converter'])[0](var)
-    
+
     ax.plot(dt_list, var)
 
     if 'time_interval' in kwargs.keys():
         ax.set_xlim(kwargs['time_interval'])
     ax.set_ylim([vmin, vmax])
-    
-    #ax.set_ylim([height_list[0], height_list[-1]])
-    #ax.set_xlim([dt_list[rect.t_bg], dt_list[rect.t_ed-1]])
-    #ax.set_ylim([range_list[rect.h_bg], range_list[rect.h_ed-1]])
+
+    # ax.set_ylim([height_list[0], height_list[-1]])
+    # ax.set_xlim([dt_list[rect.t_bg], dt_list[rect.t_ed-1]])
+    # ax.set_ylim([range_list[rect.h_bg], range_list[rect.h_ed-1]])
     ax.set_xlabel("Time UTC", fontweight='semibold', fontsize=15)
 
-    ylabel = "{} {} [{}]".format(data["system"], data["name"], data['var_unit']) 
+    ylabel = "{} {} [{}]".format(data["system"], data["name"], data['var_unit'])
     ax.set_ylabel(ylabel, fontweight='semibold', fontsize=15)
 
     ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%H:%M'))
-    #ax.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(byminute=[0,30]))
-    #ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=range(0,61,10)))
+    # ax.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(byminute=[0,30]))
+    # ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=range(0,61,10)))
     time_extend = dt_list[-1] - dt_list[0]
     print(time_extend)
     if time_extend > datetime.timedelta(hours=6):
-        ax.xaxis.set_major_locator(matplotlib.dates.HourLocator(byhour=[0,3,6,9,12,15,18,21]))
-        ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=[0,30]))
+        ax.xaxis.set_major_locator(matplotlib.dates.HourLocator(byhour=[0, 3, 6, 9, 12, 15, 18, 21]))
+        ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=[0, 30]))
     elif time_extend > datetime.timedelta(hours=1):
         ax.xaxis.set_major_locator(matplotlib.dates.HourLocator(interval=1))
-        ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=[0,15,30,45]))
+        ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=[0, 15, 30, 45]))
     else:
-        ax.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(byminute=[0,15,30,45]))
-        ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=[0,5,10,15,20,25,30,35,40,45,50,55]))
+        ax.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(byminute=[0, 15, 30, 45]))
+        ax.xaxis.set_minor_locator(
+            matplotlib.dates.MinuteLocator(byminute=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]))
 
     ax.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
 
     ax.tick_params(axis='both', which='both', right=True, top=True)
-    ax.tick_params(axis='both', which='major', labelsize=14, 
+    ax.tick_params(axis='both', which='major', labelsize=14,
                    width=3, length=5.5)
     ax.tick_params(axis='both', which='minor', width=2, length=3)
 
     return fig, ax
-
 
 
 def plot2d(data, **kwargs):
@@ -248,20 +252,20 @@ def plot2d(data, **kwargs):
     dt_list = [datetime.datetime.utcfromtimestamp(time) for time in time_list]
     # this is the last valid index
     var = var.filled(-999)
-    jumps = np.where(np.diff(time_list)>60)[0]
+    jumps = np.where(np.diff(time_list) > 60)[0]
     for ind in jumps[::-1].tolist():
-        print("jump at ", ind, dt_list[ind-1:ind+2])
+        print("jump at ", ind, dt_list[ind - 1:ind + 2])
         # and modify the dt_list
-        dt_list.insert(ind+1, dt_list[ind]+datetime.timedelta(seconds=5))
+        dt_list.insert(ind + 1, dt_list[ind] + datetime.timedelta(seconds=5))
         # add the fill array
-        var = np.insert(var, ind+1, np.full(range_list.shape, -999), axis=0)
-    
-    var = np.ma.masked_equal(var, -999) 
+        var = np.insert(var, ind + 1, np.full(range_list.shape, -999), axis=0)
+
+    var = np.ma.masked_equal(var, -999)
 
     vmin, vmax = data['var_lims']
-    print("varlims", vmin,vmax)
+    print("varlims", vmin, vmax)
     plotkwargs = {}
-    if 'z_converter' in kwargs: 
+    if 'z_converter' in kwargs:
         if kwargs['z_converter'] == 'log':
             plotkwargs['norm'] = matplotlib.colors.LogNorm(vmin=vmin, vmax=vmax)
         else:
@@ -270,12 +274,12 @@ def plot2d(data, **kwargs):
     print("custom colormaps ", VIS_Colormaps.custom_colormaps.keys())
     if colormap in VIS_Colormaps.custom_colormaps.keys():
         colormap = VIS_Colormaps.custom_colormaps[colormap]
-    
+
     fig, ax = plt.subplots(1, figsize=(10, 5.7))
     pcmesh = ax.pcolormesh(matplotlib.dates.date2num(dt_list[:]),
                            range_list[:],
                            np.transpose(var[:, :]),
-                           #cmap=VIS_Colormaps.carbonne_map, 
+                           # cmap=VIS_Colormaps.carbonne_map,
                            cmap=colormap,
                            vmin=vmin, vmax=vmax,
                            **plotkwargs
@@ -283,7 +287,7 @@ def plot2d(data, **kwargs):
 
     if 'contour' in kwargs:
         cdata = kwargs['contour']['data']
-        
+
         dt_c = [datetime.datetime.utcfromtimestamp(time) for time in cdata['ts']]
         if 'levels' in kwargs['contour']:
             cont = ax.contour(dt_c, cdata['rg'],
@@ -294,44 +298,44 @@ def plot2d(data, **kwargs):
             cont = ax.contour(dt_c, cdata['rg'],
                               np.transpose(cdata['var']),
                               linestyles='dashed', colors='black')
-        ax.clabel(cont, fontsize=10, inline=1, fmt='%1.1f',)
+        ax.clabel(cont, fontsize=10, inline=1, fmt='%1.1f', )
 
     cbar = fig.colorbar(pcmesh, fraction=0.13)
     if 'time_interval' in kwargs.keys():
         ax.set_xlim(kwargs['time_interval'])
     if 'range_interval' in kwargs.keys():
         ax.set_ylim(kwargs['range_interval'])
-    
-    #ax.set_ylim([height_list[0], height_list[-1]])
-    #ax.set_xlim([dt_list[rect.t_bg], dt_list[rect.t_ed-1]])
-    #ax.set_ylim([range_list[rect.h_bg], range_list[rect.h_ed-1]])
+
+    # ax.set_ylim([height_list[0], height_list[-1]])
+    # ax.set_xlim([dt_list[rect.t_bg], dt_list[rect.t_ed-1]])
+    # ax.set_ylim([range_list[rect.h_bg], range_list[rect.h_ed-1]])
     ax.set_xlabel("Time UTC", fontweight='semibold', fontsize=15)
     ylabel = 'Height [{}]'.format(data['rg_unit'])
     ax.set_ylabel(ylabel, fontweight='semibold', fontsize=15)
 
-    z_string = "{} {} [{}]".format(data["system"], data["name"], data['var_unit']) 
+    z_string = "{} {} [{}]".format(data["system"], data["name"], data['var_unit'])
     cbar.ax.set_ylabel(z_string, fontweight='semibold', fontsize=15)
     ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%H:%M'))
-    #ax.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(byminute=[0,30]))
-    #ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=range(0,61,10)))
+    # ax.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(byminute=[0,30]))
+    # ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=range(0,61,10)))
     time_extend = dt_list[-1] - dt_list[0]
     print(time_extend)
     if time_extend > datetime.timedelta(hours=6):
-        ax.xaxis.set_major_locator(matplotlib.dates.HourLocator(byhour=[0,3,6,9,12,15,18,21]))
-        ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=[0,30]))
+        ax.xaxis.set_major_locator(matplotlib.dates.HourLocator(byhour=[0, 3, 6, 9, 12, 15, 18, 21]))
+        ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=[0, 30]))
     elif time_extend > datetime.timedelta(hours=1):
         ax.xaxis.set_major_locator(matplotlib.dates.HourLocator(interval=1))
-        ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=[0,15,30,45]))
+        ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=[0, 15, 30, 45]))
     else:
-        ax.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(byminute=[0,15,30,45]))
-        ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(byminute=[0,5,10,15,20,25,30,35,40,45,50,55]))
+        ax.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(byminute=[0, 15, 30, 45]))
+        ax.xaxis.set_minor_locator(
+            matplotlib.dates.MinuteLocator(byminute=[0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]))
 
-    #ax.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(500))
+    # ax.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(500))
     ax.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
 
-
     ax.tick_params(axis='both', which='both', right=True, top=True)
-    ax.tick_params(axis='both', which='major', labelsize=14, 
+    ax.tick_params(axis='both', which='major', labelsize=14,
                    width=3, length=5.5)
     ax.tick_params(axis='both', which='minor', width=2, length=3)
     cbar.ax.tick_params(axis='both', which='major', labelsize=14,
@@ -342,37 +346,69 @@ def plot2d(data, **kwargs):
     return fig, ax
 
 
+def scatter(data_container1, data_container2, var_lim, **kwargs):
+    """scatter plot for variable comparison between two devices
 
-def scatter(axh, text, x, y, marker, x_min, x_max, y_min, y_max, x_lab, y_lab):
-    import matplotlib.ticker as ticker
-    from matplotlib.patches import Polygon
-    from matplotlib.collections import PatchCollection
+    Args:
+        :param data_container1: (dict) container 1st device
+        :param data_container2: (dict) container 2nd device
+        :param var_lim: x and y axis limits
+        **z_converter (string): convert var before plotting use eg 'lin2z'
+    """
+    var1_tmp = data_container1
+    var2_tmp = data_container2
 
-    # place_text(axh, [.05, 1.1], text )
-    axh.plot(x, y, marker)
-    axh.set_xlabel(x_lab)
-    axh.set_ylabel(y_lab)
-    axh.set_xlim(left=x_min, right=x_max)
-    axh.set_ylim(bottom=y_min, top=y_max)
-    axh.xaxis.set_ticks((np.arange(x_min, x_max, (x_max - x_min) / 4.0)))
-    axh.yaxis.set_ticks((np.arange(y_min, y_max, (y_max - y_min) / 4.0)))
-    axh.xaxis.set_major_formatter(ticker.FormatStrFormatter('%0.2f'))
-    axh.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.2f'))
-    axh.grid(linestyle=':')
-    # axh.legend(loc="upper right")
-    axh.set_aspect('equal', 'box')
+    combined_mask = np.logical_or(var1_tmp['mask'], var2_tmp['mask'])
 
-    # plot 1:1 line
-    N = 100
-    X = np.linspace(x_min, x_max, N)
-    axh.plot(X, X, 'k--')
+    # convert form linear units to dBZ only for reflectivity
+    if 'z_converter' in kwargs:
+        var1 = h.lin2z(var1_tmp['var'][~combined_mask].ravel())  # +4.5
+        var2 = h.lin2z(var2_tmp['var'][~combined_mask].ravel())
+    else:
+        var1 = var1_tmp['var'][~combined_mask].ravel()  # +4.5
+        var2 = var2_tmp['var'][~combined_mask].ravel()
 
-    # add patches
-    color1 = [np.divide([31, 119, 180], 255.), np.divide([255, 127, 14], 255.)]
-    patches = [Polygon([[x_min, y_min], [x_max, y_min], [x_max, y_max]], facecolor='C0', fill=True),
-               Polygon([[x_min, y_min], [x_min, y_max], [x_max, y_max]], facecolor='C1', fill=True)]
+    # create histogram plot
+    s, i, r, p, std_err = stats.linregress(var1, var2)
+    H, xedges, yedges = np.histogram2d(var1, var2, bins=120, range=[var_lim, var_lim])
 
-    p = PatchCollection(patches, alpha=0.25)
-    p.set_color(color1)
-    axh.add_collection(p)
+    X, Y = np.meshgrid(xedges, yedges)
+    fig, ax = plt.subplots(1, figsize=(5.7, 5.7))
 
+    ax.pcolormesh(X, Y, np.transpose(H), norm=matplotlib.colors.LogNorm())
+
+    ax.text(0.01, 0.93, 'slope = {:5.3f}\nintercept = {:5.3f}\nR^2 = {:5.3f}'.format(s, i, r ** 2),
+            horizontalalignment='left', verticalalignment='center', transform=ax.transAxes)
+
+    # helper lines (1:1), ...
+    add_identity(ax, color='salmon', ls='-')
+
+    if 'custom_offset_lines' in kwargs:
+        offset = np.array([kwargs['custom_offset_lines'], kwargs['custom_offset_lines']])
+        for i in [-2, -1, 1, 2]: ax.plot(var_lim, var_lim + i * offset, color='salmon', linewidth=0.7, linestyle='--')
+
+    ax.set_xlim(var_lim)
+    ax.set_ylim(var_lim)
+    ax.set_xlabel(var1_tmp['system'] + ' ' + var1_tmp['name'])
+    ax.set_ylabel(var2_tmp['system'] + ' ' + var2_tmp['name'])
+    ax.xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+    ax.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+    ax.tick_params(axis='both', which='both', right=True, top=True)
+
+    return fig, ax
+
+
+def add_identity(axes, *line_args, **line_kwargs):
+    identity, = axes.plot([], [], *line_args, **line_kwargs)
+
+    def callback(axes):
+        low_x, high_x = axes.get_xlim()
+        low_y, high_y = axes.get_ylim()
+        low = max(low_x, low_y)
+        high = min(high_x, high_y)
+        identity.set_data([low, high], [low, high])
+
+    callback(axes)
+    axes.callbacks.connect('xlim_changed', callback)
+    axes.callbacks.connect('ylim_changed', callback)
+    return axes
