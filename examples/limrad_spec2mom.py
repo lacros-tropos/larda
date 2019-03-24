6086f57dae15189febd22d5470fb9e0b18bfeb21
willi <willi.schimmel@uni-leipzig.de>
2019-03-24 14:41:04 -0300

    reformated file and finished docstring for limrad_spec2mom.py

--- examples/limrad_spec2mom.py
+++ examples/limrad_spec2mom.py
@@ -11,7 +11,7 @@ Example:
 
 """
 
-import sys, time
+import sys, datetime
 
 sys.path.append('../')
 sys.path.append('.')
@@ -21,36 +21,52 @@ import pyLARDA.helpers as h
 import pyLARDA.SpecToMom_Util as stm
 import pyLARDA.NcWrite as nc
 
-import datetime
 import logging
 
 import numpy as np
 
 
-def make_container_from_spectra(spectra_all_chirps, variable, var_name):
+def make_container_from_spectra(spectra_all_chirps, values, paraminfo):
+    """
+    This routine will generate a larda container from calculated moments from spectra.
+
+    Args:
+        spectra_all_chirps (dict): dimension [nchirps], containing the spectrum values of the 94 GHz RPG cloud radar
+        values (numpy array): dimension [nrange, ntimes], values of calculated moments
+        paraminfo (dict): information from params_[campaign].toml for the specific variable
+
+    Return:
+        container (dict): larda data container
+    """
     import copy
 
-    container = dict()
     spectra = spectra_all_chirps[0]
 
-    container['dimlabel'] = ['time', 'range']
-    container['filename'] = spectra['filename']
-    container['paraminfo'] = copy.deepcopy(spectra['paraminfo'])
-    container['rg_unit'] = spectra['rg_unit']
-    container['colormap'] = spectra['colormap']
-    container['var_unit'] = ''
-    container['var_lims'] = ''
-    container['system'] = spectra['system']
-    container['name'] = var_name
-    container['rg'] = np.array([rg for ic in spectra_all_chirps for rg in ic['rg']])
-    container['ts'] = spectra['ts']
-    container['mask'] = np.isnan(variable[:])
-    container['var'] = variable[:]
+    container = {'dimlabel': ['time', 'range'], 'filename': spectra['filename'], 'paraminfo': copy.deepcopy(paraminfo),
+                 'rg_unit': paraminfo['rg_unit'], 'colormap': paraminfo['colormap'], 'var_unit': paraminfo['var_unit'],
+                 'var_lims': paraminfo['var_lims'], 'system': paraminfo['system'], 'name': paraminfo['paramkey'],
+                 'rg': np.array([rg for ic in spectra_all_chirps for rg in ic['rg']]), 'ts': spectra['ts'],
+                 'mask': np.isnan(values[:]), 'var': values[:]}
 
     return container
 
 
-def calculate_moments_from_spectra_rpgfmcw94(begin_dt, end_dt, nsd, png_path):
+def calculate_moments_from_spectra_rpgfmcw94(begin_dt, end_dt, nsd, paraminfo):
+    """
+    This routine calculates the radar moments: reflectivity, mean Doppler velocity, spectrum width, skewness and
+    kurtosis from the level 0 spectrum files of the 94 GHz RPG cloud radar.
+
+    Args:
+        begin_dt (datetime object): beginning of the time frame
+        end_dt (datetime object): end of the time frame
+        nsd (float): number of standard deviations above mean noise threshold
+        paraminfo (dict): information from params_[campaign].toml for the system LIMRAD94
+
+    Return:
+        container_dict (dict): dictionary of larda containers, including larda container for Ze, VEL, sw, skew, kurt
+
+    """
+
     AvgNum_in = larda.read("LIMRAD94", "AvgNum", [begin_dt, end_dt])
     DoppLen_in = larda.read("LIMRAD94", "DoppLen", [begin_dt, end_dt])
     MaxVel_in = larda.read("LIMRAD94", "MaxVel", [begin_dt, end_dt])
@@ -67,13 +83,15 @@ def calculate_moments_from_spectra_rpgfmcw94(begin_dt, end_dt, nsd, png_path):
 
     n_chirps = len(AvgNum)
 
+    cum_rg = [0]
     LIMRAD_Zspec = []
     for ic in range(n_chirps):
-        LIMRAD_Zspec.append(larda.read("LIMRAD94", "C" + str(ic + 1) + "VSpec", [begin_dt, end_dt], [0, 'max']))
+        LIMRAD_Zspec.append(larda.read("LIMRAD94", "C{}VSpec".format(ic + 1), [begin_dt, end_dt], [0, 'max']))
         LIMRAD_Zspec[ic].update({'no_av': np.divide(AvgNum[ic], DoppLen[ic]), 'DoppRes': DoppRes[ic]})
+        cum_rg.append(cum_rg[ic] + LIMRAD_Zspec[ic]['rg'].size)
 
     # Logicals for different tasks
-    include_noise = False
+    include_noise = True if nsd < 0.0 else False
 
     # remove noise from raw spectra and calculate radar moments
     noise_est = stm.noise_estimation(LIMRAD_Zspec, n_std_deviations=nsd, include_noise=include_noise)
@@ -85,15 +103,15 @@ def calculate_moments_from_spectra_rpgfmcw94(begin_dt, end_dt, nsd, png_path):
     #       -   noise_est[:]['signal']      [Nchirps][ntime, nrange]
     #       -   noise_est[:]['threshold']   [Nchirps][ntime, nrange]
 
-    LIMRAD_moments = stm.spectra_to_moments_rpgfmcw94(LIMRAD_Zspec, noise_est)
+    LIMRAD_moments = stm.spectra_to_moments_rpgfmcw94(LIMRAD_Zspec, noise_est, include_noise=include_noise)
 
-    container = {'Ze': make_container_from_spectra(LIMRAD_Zspec, LIMRAD_moments['Ze_lin'].T, 'Ze'),
-                 'VEL': make_container_from_spectra(LIMRAD_Zspec, LIMRAD_moments['VEL'].T, 'VEL'),
-                 'sw': make_container_from_spectra(LIMRAD_Zspec, LIMRAD_moments['sw'].T, 'sw'),
-                 'skew': make_container_from_spectra(LIMRAD_Zspec, LIMRAD_moments['skew'].T, 'skew'),
-                 'kurt': make_container_from_spectra(LIMRAD_Zspec, LIMRAD_moments['kurt'].T, 'kurt')}
+    stm.noise_pixel_filter(LIMRAD_moments, cum_rg)
 
-    return container
+    container_dict = {}
+    for mom in ['Ze', 'VEL', 'sw', 'skew', 'kurt']:
+        container_dict.update({mom: make_container_from_spectra(LIMRAD_Zspec, LIMRAD_moments[mom].T, paraminfo[mom])})
+
+    return container_dict
 
 
 if __name__ == '__main__':
@@ -119,24 +137,92 @@ if __name__ == '__main__':
         begin_dt = datetime.datetime.strptime(date + ' 12:30:05', '%Y%m%d %H:%M:%S')
         end_dt = datetime.datetime.strptime(date + ' 14:15:08', '%Y%m%d %H:%M:%S')
     else:
-        date = '20190110'
-        begin_dt = datetime.datetime.strptime(date + ' 13:10:05', '%Y%m%d %H:%M:%S')
-        end_dt = datetime.datetime.strptime(date + ' 14:15:08', '%Y%m%d %H:%M:%S')
+        date = '20190324'
+        begin_dt = datetime.datetime.strptime(date + ' 2:00:05', '%Y%m%d %H:%M:%S')
+        end_dt = datetime.datetime.strptime(date + ' 2:15:08', '%Y%m%d %H:%M:%S')
 
     std_above_mean_noise = float(kwargs['NF']) if 'NF' in kwargs else 6.0
     png_path = 'spec2mom_diff_NF/'
 
-    LIMRAD94_vars = calculate_moments_from_spectra_rpgfmcw94(begin_dt, end_dt, std_above_mean_noise, png_path)
+    LIMRAD94_vars = calculate_moments_from_spectra_rpgfmcw94(begin_dt, end_dt, std_above_mean_noise,
+                                                             larda.connectors['LIMRAD94'].system_info['params'])
 
-    variable_list = ['DiffAtt', 'ldr', 'bt', 'rr', 'LWP',
-                     'MaxVel', 'C1Range', 'C2Range', 'C3Range']
+########################################################################################################################
+#
+#   Plotting radar moments for quicklooks
+#
+#    #LIMRAD94_ZE = larda.read("LIMRAD94", "Ze", [begin_dt, end_dt], [0, 'max'])
+#    LIMRAD94_ZE = LIMRAD94_vars['Ze']
+#    LIMRAD94_ZE['var_unit'] = 'dBZ'
+#    fig, axZe = pyLARDA.Transformations.plot_timeheight(LIMRAD94_ZE, fig_size=[16, 3], range_interval=[0, 12000],
+#                                                        z_converter='lin2z', title='nofilter')
+#    fig_name = 'limrad_Ze' + f'{begin_dt:%Y%m%d_%H%M%S_}{end_dt:%H%M%S_}' + '0-12000' + '.png'
+#    fig.savefig(fig_name, dpi=250)
+#
+#    LIMRAD94_VEL = LIMRAD94_vars['VEL']
+#    fig, axZe = pyLARDA.Transformations.plot_timeheight(LIMRAD94_VEL, fig_size=[16, 3], range_interval=[0, 12000])
+#    fig_name = 'limrad_VEL' + f'{begin_dt:%Y%m%d_%H%M%S_}{end_dt:%H%M%S_}' + '0-12000' + '.png'
+#    fig.savefig(fig_name, dpi=250)
+#
+#    LIMRAD94_sw = LIMRAD94_vars['sw']
+#    fig, axZe = pyLARDA.Transformations.plot_timeheight(LIMRAD94_sw, fig_size=[16, 3], range_interval=[0, 12000])
+#    fig_name = 'limrad_sw' + f'{begin_dt:%Y%m%d_%H%M%S_}{end_dt:%H%M%S_}' + '0-12000' + '.png'
+#    fig.savefig(fig_name, dpi=250)
+########################################################################################################################
 
-    for var in variable_list:
-        print('variable :: ' + var)
-        # if var in ["Ze", "VEL", 'sw', "skew", 'kurt', 'DiffAtt', 'ldr']:
-        #    kwargs = {'interp_rg_join': True}
-        # else:
-        kwargs = {}
-        LIMRAD94_vars.update({var: larda.read("LIMRAD94", var, [begin_dt, end_dt], [0, 'max'], **kwargs)})
 
-    flag = nc.generate_cloudnet_input_LIMRAD94(LIMRAD94_vars, png_path, time_frame='141005-141508')
+
+########################################################################################################################
+#
+#   Plotting sensitivity limits for different dates
+#
+#    # load sensitivity limits (time, height) and calculate the mean over time
+#    begin_dt = datetime.datetime.strptime(date + ' 1:00:05', '%Y%m%d %H:%M:%S')
+#    end_dt = datetime.datetime.strptime(date + ' 13:00:08', '%Y%m%d %H:%M:%S')
+#    LIMRAD94_SLv_nf5 = larda.read("LIMRAD94", "SLv", [begin_dt, end_dt], [0, 12000])
+#    sens_lim_nf5 = np.mean(LIMRAD94_SLv_nf5['var'], axis=0)
+#
+#    date = '20190102'
+#    begin_dt = datetime.datetime.strptime(date + ' 0:00:05', '%Y%m%d %H:%M:%S')
+#    end_dt = datetime.datetime.strptime(date + ' 23:50:08', '%Y%m%d %H:%M:%S')
+#    LIMRAD94_SLv_nf6 = larda.read("LIMRAD94", "SLv", [begin_dt, end_dt], [0, 12000])
+#    sens_lim_nf6 = np.mean(LIMRAD94_SLv_nf6['var'], axis=0)
+#
+#    import matplotlib.pyplot as plt
+#
+#    fig, ax = plt.subplots(1, figsize=(13, 8))
+#
+#    sens_lim_nf5 = h.lin2z(sens_lim_nf5)
+#    sens_lim_nf6 = h.lin2z(sens_lim_nf6)
+#
+#    y_bins_nf5 = LIMRAD94_SLv_nf5['rg']
+#    y_bins_nf6 = LIMRAD94_SLv_nf6['rg']
+#    ax.plot(sens_lim_nf5, y_bins_nf5, linewidth=2.0, color='red', label='sensitivity limit noisefactor 5')
+#    ax.plot(sens_lim_nf6, y_bins_nf6, linewidth=2.0, color='blue', label='sensitivity limit noisefactor 6')
+#
+#    plt.grid(b=True, which='major', color='black', linestyle='--', linewidth=0.5, alpha=0.5)
+#    plt.grid(b=True, which='minor', color='gray', linestyle=':', linewidth=0.25, alpha=0.5)
+#    plt.legend(loc='upper left')
+#
+#    ax.set_xlim([-60, -20])
+#    ax.set_ylim([0, 12000])
+#
+#    fig_name = 'limrad_senslimit_nf5_nf6' + '0-12000' + '.png'
+#    fig.savefig(fig_name, dpi=250)
+########################################################################################################################
+
+
+########################################################################################################################
+#
+#   Generating calibrated netcdf file
+#
+#    for var in ['DiffAtt', 'ldr', 'bt', 'rr', 'LWP', 'MaxVel', 'C1Range', 'C2Range', 'C3Range']:
+#        print('variable :: ' + var)
+#        # if var in ["Ze", "VEL", 'sw', "skew", 'kurt', 'DiffAtt', 'ldr']:
+#        #    kwargs = {'interp_rg_join': True}
+#        # else:
+#        kwargs = {}
+#        LIMRAD94_vars.update({var: larda.read("LIMRAD94", var, [begin_dt, end_dt], [0, 'max'], **kwargs)})
+#
+#    flag = nc.generate_cloudnet_input_LIMRAD94(LIMRAD94_vars, png_path, time_frame='141005-141508')
+########################################################################################################################
