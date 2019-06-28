22312addb901e0b6e190f35451e996408c8ad2a5
willi <willi.schimmel@uni-leipzig.de>
2019-06-28 14:43:19 +0200

    update to Spec2Mom utility:   - added option to just use the main peak of a signal for moment calculation   - added doc string to despeckle routine   - outsourced ghost echo filter into this python file   - added routine to generate larda container out of calculated numpy arrays   - added routine to generate larda container out of predicted ANN output_

--- pyLARDA/SpecToMom_Util.py
+++ pyLARDA/SpecToMom_Util.py
@@ -88,6 +88,31 @@ def estimate_noise_hs74(spectrum, navg=1.0, std_div=-1.0):
     return mean, threshold, var, nnoise, signal_flag, left_intersec, right_intersec
 
 
+@jit(nopython=True)
+def find_main_peak(signal):
+    idxMaxSignal = np.argmax(signal)
+    thresh = np.min(signal)
+    len_sig = len(signal)
+
+    for ispec in range(idxMaxSignal, len_sig):
+        if signal[ispec] < thresh:
+            right_intersec = ispec
+            break
+        else:
+            # strong signal till max Nyquist Velocity, maybe folding?
+            right_intersec = -222
+
+    for ispec in range(idxMaxSignal, -1, -1):
+        if signal[ispec] < thresh:
+            left_intersec = ispec
+            # strong signal till max Nyquist Velocity, maybe folding?
+            break
+        else:
+            left_intersec = -222
+
+    return thresh, left_intersec, right_intersec
+
+
 #
 def noise_estimation(data, **kwargs):
     """
@@ -107,8 +132,10 @@ def noise_estimation(data, **kwargs):
 
     n_std = kwargs['n_std_deviations'] if 'n_std_deviations' in kwargs else 1.0
     include_noise = kwargs['include_noise'] if 'include_noise' in kwargs else False
+    main_peak = kwargs['main_peak'] if 'main_peak' in kwargs else False
 
     n_chirps = len(data)
+    n_dbin = data[0]['var'].shape[2] // 2
 
     # fill values needs to be masked for noise removal otherwise wrong results
     for ic in range(n_chirps):
@@ -137,7 +164,8 @@ def noise_estimation(data, **kwargs):
             # gather noise level etc. for all chirps, range gates and times
             for iR in range(n_r):
                 for iT in range(n_t):
-                    if not any(np.isnan(data[ic]['var'][iT, iR, :])):
+                    nonoise = any(np.isnan(data[ic]['var'][iT, iR, :]))
+                    if not nonoise:
                         mean, thresh, var, nnoise, signal, left, right = \
                             estimate_noise_hs74(data[ic]['var'][iT, iR, :], navg=data[ic]['no_av'], std_div=n_std)
 
@@ -146,6 +174,20 @@ def noise_estimation(data, **kwargs):
                         noise_est[ic]['numnoise'][iT, iR] = nnoise
                         noise_est[ic]['threshold'][iT, iR] = thresh
                         noise_est[ic]['signal'][iT, iR] = signal
+                        noise_est[ic]['bounds'][iT, iR, :] = [left, right]
+
+                    elif main_peak and not nonoise:
+
+                        # filter for c1 ghost echos?
+                        # sum_SaN_neg = np.count_nonzero(~np.isnan(data[ic]['var'][iT, iR, :n_dbin]))
+                        # sum_SaN_pos = np.count_nonzero(~np.isnan(data[ic]['var'][iT, iR, n_dbin:]))
+                        # if sum_SaN_neg < 10 and sum_SaN_pos < 8 and ic == 0:
+                        #    thresh, left, right = np.min(data[ic]['var'][iT, iR, :]), -222, -222
+                        #    data[ic]['var'][iT, iR, :] = np.nan
+                        # else:
+                        thresh, left, right = find_main_peak(data[ic]['var'][iT, iR, :])
+
+                        noise_est[ic]['threshold'][iT, iR] = thresh
                         noise_est[ic]['bounds'][iT, iR, :] = [left, right]
 
             noise_est[ic]['bounds'][noise_est[ic]['bounds'] == -222] = None
@@ -183,13 +225,12 @@ def spectra_to_moments_rpgfmcw94(spectrum_container, noise_est, **kwargs):
     no_times = spectrum_container[0]['ts'].size
     cum_rg = [0]
     no_ranges_tot = 0
-    ic = 0
-    for ichirp in spectrum_container:
+    for ic, ichirp in enumerate(spectrum_container):
         cum_rg.append(cum_rg[ic] + ichirp['rg'].size)
         no_ranges_tot += ichirp['rg'].size
-        ic += 1
 
     include_noise = kwargs['include_noise'] if 'include_noise' in kwargs else False
+    main_peak = kwargs['main_peak'] if 'main_peak' in kwargs else False
 
     # initialize variables:
     moments = {'Ze': np.full((no_ranges_tot, no_times), np.nan),
@@ -199,7 +240,6 @@ def spectra_to_moments_rpgfmcw94(spectrum_container, noise_est, **kwargs):
                'kurt': np.full((no_ranges_tot, no_times), np.nan),
                'mask': np.full((no_ranges_tot, no_times), True)}
 
-
     for ic in range(n_chirps):
         tstart = time.time()
         no_ranges = spectrum_container[ic]['rg'].size
@@ -208,7 +248,7 @@ def spectra_to_moments_rpgfmcw94(spectrum_container, noise_est, **kwargs):
         velocity_bins = spectrum_container[ic]['vel']
         DoppRes = spectrum_container[ic]['DoppRes']
 
-        #if ic == 3:
+        # if ic == 3:
         #    mask = spectra_linear_units < h.lin2z(25.0) and
         #    spectra_linear_units[] = spectra_linear_units
 
@@ -222,10 +262,17 @@ def spectra_to_moments_rpgfmcw94(spectrum_container, noise_est, **kwargs):
 
                 else:
                     Ze_lin, VEL, sw, skew, kurt = [np.nan] * 5
+
                     if noise_est[ic]['signal'][iT, iR]:
-                        spec_no_noise = copy.deepcopy(spectra_linear_units[iT, iR, :])
-                        spec_no_noise[spectra_linear_units[iT, iR, :] < noise_est[ic]['threshold'][iT, iR]] = np.nan
-                        Ze_lin, VEL, sw, skew, kurt = moment_calculation(spec_no_noise, velocity_bins, DoppRes)
+                        if main_peak:
+                            lb, rb = noise_est[ic]['bounds'][iT, iR, :]
+                            spec_no_noise = copy.deepcopy(spectra_linear_units[iT, iR, lb:rb])
+                            velocity_bins_extr = velocity_bins[lb:rb]
+                            Ze_lin, VEL, sw, skew, kurt = moment_calculation(spec_no_noise, velocity_bins_extr, DoppRes)
+                        else:
+                            spec_no_noise = copy.deepcopy(spectra_linear_units[iT, iR, :])
+                            spec_no_noise[spectra_linear_units[iT, iR, :] < noise_est[ic]['threshold'][iT, iR]] = np.nan
+                            Ze_lin, VEL, sw, skew, kurt = moment_calculation(spec_no_noise, velocity_bins, DoppRes)
 
                 iR_tot = cum_rg[ic] + iR
 
@@ -234,7 +281,6 @@ def spectra_to_moments_rpgfmcw94(spectrum_container, noise_est, **kwargs):
                 moments['sw'][iR_tot, iT] = sw
                 moments['skew'][iR_tot, iT] = skew
                 moments['kurt'][iR_tot, iT] = kurt
-
 
         print('moments calculated, chrip = {}, elapsed time = {:.3f} sec.'.format(ic + 1, time.time() - tstart))
 
@@ -317,49 +363,127 @@ def moment_calculation(signal, vel_bins, DoppRes):
 
     return Ze_lin, VEL, sw, skew, kurt
 
+
+def filter_ghost_echos_RPG94GHz_FMCW(data, **kwargs):
+    ######################################################################
+    #
+    # 3rd chirp ghost echo filter
+    if 'C2C3' in kwargs and kwargs['C2C3']:
+        tstart = time.time()
+
+        for ichirp in [1, 2]:
+
+            # threholds for 3rd chrip ghost echo filter
+            new_ny_vel = data[ichirp]['vel'].max() - 2.5
+
+            ic_Ze_max = h.z2lin(-22.5)
+
+            idx_left = np.argwhere(-new_ny_vel > data[ichirp]['vel']).max()
+            idx_right = np.argwhere(new_ny_vel < data[ichirp]['vel']).min()
+
+            Ze_lin_left = data[ichirp]['var'][:, :, :idx_left].copy()
+            Ze_lin_right = data[ichirp]['var'][:, :, idx_right:].copy()
+
+            # if noise was already removed by the RPG software, replace the ghost with -999.,
+            # if noise factor 0 was selected in the RPG software, replace the ghost by the minimum noise value, to avoid
+            # wrong noise estimations (to much signal would be lost otherwise),
+            idx_ts_nf0 = np.argwhere(data[ichirp]['var'][:, 0, 0] != -999.0)
+
+            if idx_ts_nf0.size > 0:
+                mask_left, mask_right = Ze_lin_left < ic_Ze_max, Ze_lin_right < ic_Ze_max
+                min_left, min_right = np.amin(Ze_lin_left, axis=2), np.amin(Ze_lin_right, axis=2)
+
+                for i_bin in range(mask_left.shape[2]):
+                    Ze_lin_left[mask_left[:, :, i_bin], i_bin] = min_left[mask_left[:, :, i_bin]]
+                for i_bin in range(mask_right.shape[2]):
+                    Ze_lin_right[mask_right[:, :, i_bin], i_bin] = min_right[mask_right[:, :, i_bin]]
+            else:
+                Ze_lin_left[Ze_lin_left < ic_Ze_max] = -999.0
+                Ze_lin_right[Ze_lin_right < ic_Ze_max] = -999.0
+
+            data[ichirp]['var'][:, :, :idx_left] = Ze_lin_left.copy()
+            data[ichirp]['var'][:, :, idx_right:] = Ze_lin_right.copy()
+            print(
+                'filtered ghost echos in chirp {}, elapsed time = {:.3f} sec.'.format(ichirp + 1, time.time() - tstart))
+
+        ######################################################################
+        #
+    if 'C1' in kwargs and kwargs['C1']:
+        tstart = time.time()
+
+        # invalid mask has to be provided via kwarg for this filter
+        invalid_mask = kwargs['inv_mask']
+        rg_offsets = kwargs['offset']
+        SLv = kwargs['SL']
+
+        # setting higher threshold if chirp 2 contains high reflectivity values
+        #sum_over_heightC1 = np.ma.sum(data['Ze'][:rg_offsets[1], :], axis=0)
+        sum_over_heightC2 = np.ma.sum(data['Ze'][rg_offsets[1]:rg_offsets[2], :], axis=0)
+
+        # load sensitivity limits (time, height) and calculate the mean over time
+        sens_reduction = 15.0  # sensitivity in chirp 1 is reduced by 12.0 dBZ
+        C2_Ze_threshold = 18.0  # if sum(C2_Ze) > this threshold, ghost echo in C1 is assumed
+
+        sens_lim = h.z2lin(h.lin2z(np.mean(SLv['var'], axis=0)) + sens_reduction)[:rg_offsets[1]]
+        ts_to_mask = np.argwhere(h.lin2z(sum_over_heightC2) > C2_Ze_threshold)[:, 0]
+
+        m1 = invalid_mask[:rg_offsets[1], :].copy()
+        for idx_ts in ts_to_mask:
+            m1[:, idx_ts] = data['Ze'][:rg_offsets[1], idx_ts] < sens_lim
+            # a = data['Ze'][:rg_offsets[1], idx_ts] < sens_lim
+            # b = data['VEL'][:rg_offsets[1], idx_ts] < -0.5
+            # m1[:, idx_ts] = a == b
+
+        invalid_mask[:rg_offsets[1], :] = m1.copy()
+
+        for mom in ['Ze', 'VEL', 'sw', 'skew', 'kurt']:
+            data[mom][:rg_offsets[1], :] = np.ma.masked_where(m1, data[mom][:rg_offsets[1], :])
+
+        print('filtered ghost echos in chirp 1, elapsed time = {:.3f} sec.'.format(time.time() - tstart))
+
+        return invalid_mask
+
+
 @jit(nopython=True, fastmath=True)
 def despeckle(mask, n_neighbours):
     """
-    % SPECKLEFILTER
-    %
-    % last modification: Heike Kalesse, April 26, 2017; kalesse@tropos.de
-    %
-    % TBD:
-    % - define percentage of neighboring points that have to be 1 in order to keep pxl value as 1 (instead of "hard" number)
-    % - what is "C" (in output?)
-    %
-    % functionality:
-    % --------------
-    %   remove small patches (speckle) from any given mask by checking 5x5 box
-    %   around each pixel, more than half of the points in the box need to be 1
-    %   to keep the 1 at current pixel
-    %
-    % input:
-    % ------
-    %   mask         ... mask where 1 = an invalid/fill value and 0 = a data point [height x time]
-    %   nr_neighbors ... number of neighbors of pixel that have to be 1 in order to keep pixel value as 1
+    SPECKLEFILTER
 
-    %
-    % output:
-    % -------
-    %   mask2 ... speckle-filtered matrix of 0 and 1 that represents (cloud) mask [height x time]
+    last modification: Heike Kalesse, April 26, 2017; kalesse@tropos.de
 
-    % example of a proggi using this function:
-    % % % % filter out speckles of liq (this is done later in the Shupe 2007 algorithm)
-    % % % nr_neighbors = 15;  % number of neighbors of pixel (in a 5x5 matrix; i.e., 25pxl) that have to be 1 in order to keep pixel value as 1 in "speckleFilter.m" (orig=12)
-    % % % [liq_mask,C] = speckleFilter(liq_mask, nr_neighbors);
-    % 20 neighbors in 5x5 matrix means 80%
+    TBD:
+    - define percentage of neighboring points that have to be 1 in order to keep pxl value as 1 (instead of "hard" number)
+    - what is "C" (in output?)
+
+    functionality:
+        remove small patches (speckle) from any given mask by checking 5x5 box
+        around each pixel, more than half of the points in the box need to be 1
+        to keep the 1 at current pixel
+
+    Args:
+        mask         ... mask where 1 = an invalid/fill value and 0 = a data point [height x time]
+        nr_neighbors ... number of neighbors of pixel that have to be 1 in order to keep pixel value as 1
+
+
+    Return:
+        mask2 ... speckle-filtered matrix of 0 and 1 that represents (cloud) mask [height x time]
+
+    example of a proggi using this function:
+    % % % filter out speckles of liq (this is done later in the Shupe 2007 algorithm)
+    % % nr_neighbors = 15;  % number of neighbors of pixel (in a 5x5 matrix; i.e., 25pxl) that have to be 1 in order to keep pixel value as 1 in "speckleFilter.m" (orig=12)
+    % % [liq_mask,C] = speckleFilter(liq_mask, nr_neighbors);
+    20 neighbors in 5x5 matrix means 80%
     """
 
-    window_size = 5     # 5x5 window
-    shift = int(window_size/2)
+    window_size = 5  # 5x5 window
+    shift = int(window_size / 2)
     n_rg, n_ts = mask.shape
 
     for iR in range(n_rg - window_size):
         for iT in range(n_ts - window_size):
 
             if mask[iR, iT] == 1:
-                # selecting window of 3x3 pixels
+                # selecting window of 5x5 pixels
                 window = mask[iR:iR + window_size, iT:iT + window_size]
 
                 # if more than n_neighbours pixel in the window are fill values, remove the pixel in the middle
@@ -367,3 +491,60 @@ def despeckle(mask, n_neighbours):
                     mask[iR + shift, iT + shift] = 1
 
     return mask
+
+
+def make_container_from_spectra(spectra_all_chirps, values, paraminfo, invalid_mask):
+    """
+    This routine will generate a larda container from calculated moments from spectra.
+
+    Args:
+        spectra_all_chirps (list of dicts): dimension [nchirps], containing the spectrum
+                                            values of the 94 GHz RPG cloud radar
+        values (numpy array): dimension [nrange, ntimes], values of calculated moments
+        paraminfo (dict): information from params_[campaign].toml for the specific variable
+
+    Return:
+        container (dict): larda data container
+    """
+
+    spectra = spectra_all_chirps[0]
+
+    container = {'dimlabel': ['time', 'range'], 'filename': spectra['filename'], 'paraminfo': copy.deepcopy(paraminfo),
+                 'rg_unit': paraminfo['rg_unit'], 'colormap': paraminfo['colormap'],
+                 'var_unit': paraminfo['var_unit'],
+                 'var_lims': paraminfo['var_lims'],
+                 'system': paraminfo['system'], 'name': paraminfo['paramkey'],
+                 'rg': np.array([rg for ic in spectra_all_chirps for rg in ic['rg']]), 'ts': spectra['ts'],
+                 'mask': invalid_mask, 'var': values[:]}
+
+    return container
+
+
+def make_container_from_prediction(radar, pred_list, paraminfo, ts=0, rg=0, **kwargs):
+    pred_var = np.full((ts.size, rg.size), fill_value=-999.0)
+    cnt = 0
+    for iT, iR in zip(radar[:, 0], radar[:, 1]):
+        iT, iR = int(iT), int(iR)
+        pred_var[iT, iR] = pred_list[cnt]
+        # print(iT, iR, pred_list[cnt], pred_var[iT, iR])
+        cnt += 1
+
+    mask = np.full((ts.size, rg.size), fill_value=False)
+    mask[pred_var <= -999.0] = True
+    pred_var = np.ma.masked_less_equal(pred_var, -999.0)
+
+    container = {'dimlabel': ['time', 'range'],
+                 'filename': [],
+                 'paraminfo': copy.deepcopy(paraminfo),
+                 'rg_unit': paraminfo['rg_unit'],
+                 'colormap': paraminfo['colormap'],
+                 'var_unit': paraminfo['var_unit'],
+                 'var_lims': paraminfo['var_lims'],
+                 'system': paraminfo['system'],
+                 'name': paraminfo['paramkey'],
+                 'rg': rg.copy(),
+                 'ts': ts.copy(),
+                 'mask': mask,
+                 'var': pred_var}
+
+    return container
