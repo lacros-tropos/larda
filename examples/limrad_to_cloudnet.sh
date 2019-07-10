8e30d333b8ee0769f66580fc40d6d8e8b0ab018c
ti-vo <teresa.vogl@tropos.de>
2019-07-10 17:08:48 +0200

    combined two mask approaches for kazr spectra Author: tv
    Signed-off-by: ti-vo <teresa.vogl@tropos.de>

--- examples/limrad_to_cloudnet.sh
+++ examples/limrad_to_cloudnet.sh
@@ -1,5 +1,5 @@
 d=20190101
 while [ "$d" != 20190131 ]
     do python LIMRAD94_to_Cloudnet.py date=${d} path=/lacroshome/remsens_lim/data/cloudnet/punta-arenas/calibrated/limrad94/2019/
-    d=$(date -d "$d + 1 day" '+%Y%m%d') 
+    d=$(date -d "$d + 1 day" '+%Y%m%d')
 done
