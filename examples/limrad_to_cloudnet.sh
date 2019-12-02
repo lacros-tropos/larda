79c0dbd7f3297dd2c9141b3d2cd3fa75bb5b6d1b
willi <willi.schimmel@uni-leipzig.de>
2019-12-02 12:04:25 +0100

    removed deprecated LIMRAD94_to_Cloudnet.py incl. shell script from examples folder

--- examples/limrad_to_cloudnet.sh
+++
@@ -1,5 +0,0 @@
-d=20190101
-while [ "$d" != 20190131 ]
-    do python LIMRAD94_to_Cloudnet_v2.py date=${d} path=/lacroshome/remsens_lim/data/cloudnet/punta-arenas/calibrated/limrad94/2019/
-    d=$(date -d "$d + 1 day" '+%Y%m%d')
-done
