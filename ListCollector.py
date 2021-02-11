5c4eb7653bd4998a8aafc67f155514b0de568c5d
larda3_rsd2 <larda3@rsd2.tropos.de>
2021-02-11 10:59:54 +0000

    add the option to specify campaigns at ListCollector

--- ListCollector.py
+++ ListCollector.py
@@ -9,6 +9,7 @@ Author: radenz@tropos.de
 
 import pyLARDA
 import os
+import argparse
 
 import logging
 log = logging.getLogger('pyLARDA')
@@ -18,9 +19,22 @@ log.addHandler(logging.StreamHandler())
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 print(ROOT_DIR)
 
+parser = argparse.ArgumentParser(
+    description='''
+    Example `python3 ListCollector.py -c oceanet_pascal lacros_accept`.
+    As default all campaigns are collected.'''
+)
+parser.add_argument('-c', '--campaign', nargs='+',
+                    help='just run for a defined campaign(s)')
+args = parser.parse_args()
+
 camp = pyLARDA.LARDA_campaign(ROOT_DIR + "/../larda-cfg/", "campaigns.toml")
 camp_list = camp.get_campaign_list()
 print(camp_list)
+
+if args.campaign:
+    assert set(args.campaign).issubset(camp_list), 'campaign not in list'
+    camp_list = args.campaign
 
 #larda=pyLARDA.LARDA().connect('test_filepatterns', build_lists=True)
 
