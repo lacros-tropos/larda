6794ef467702e6ce9cd920f672c54c5689b1ccd1
martin_rdz <31452472+martin-rdz@users.noreply.github.com>
2024-04-06 06:27:28 +0200

    In setup.py drop the hard numpy requirement

--- setup.py
+++ setup.py
@@ -39,9 +39,9 @@ setup(
     python_requires='>=3.8',
     # automatic installation of the dependencies did not work with the test.pypi
     # below the try to fix it
-    setup_requires=['wheel', 'numpy==1.22.0', 'scipy>=1.6', 'netCDF4>=1.4.2', 'msgpack', 'cython>=0.29.13', 'xarray',
+    setup_requires=['wheel', 'numpy', 'scipy>=1.6', 'netCDF4>=1.4.2', 'msgpack', 'cython>=0.29.13', 'xarray',
                       'matplotlib>=3.0.2', 'requests>=2.21', 'toml>=0.10.0', 'tqdm>=4.36.1', 'numba>=0.45.1'],
-    install_requires=['numpy==1.22.0', 'scipy>=1.6', 'netCDF4>=1.4.2', 'msgpack', 'cython>=0.29.13', 'xarray',
+    install_requires=['numpy', 'scipy>=1.6', 'netCDF4>=1.4.2', 'msgpack', 'cython>=0.29.13', 'xarray',
                       'matplotlib>=3.0.2', 'requests>=2.21', 'toml>=0.10.0', 'tqdm>=4.36.1', 'numba>=0.45.1'],
     ext_modules=cythonize("pyLARDA/peakTree_fastbuilder.pyx"),
     classifiers=[
