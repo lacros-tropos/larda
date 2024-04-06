cd2e26cd1850d4c8c2ee5a0a7e55d7c34f245bfc
radenz@tropos.de <radenz@tropos.de>
2024-04-06 05:38:45 +0000

    modified version number and added pyproject

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     author=meta['__author__'],
     author_email='radenz@tropos.de',
     url='https://github.com/lacros-tropos/larda',
-    download_url='https://github.com/lacros-tropos/larda/archive/refs/tags/v3.3.5.tar.gz',
+    download_url='https://github.com/lacros-tropos/larda/archive/refs/tags/v3.3.6.tar.gz',
     license='MIT License',
     packages=['pyLARDA'],
     #package_data={}, # include via MANIFEST.in
